d8b36b4bbb26d02d059fbe1222c3c4939da4db38
anywhere <weilizong2014@hotmail.com>
2019-03-18 21:43:59 +0800

    增加mysql连接

--- src/main/java/com/delta/soft_manage_system/controller/TestController.java
+++ src/main/java/com/delta/soft_manage_system/controller/TestController.java
@@ -1,16 +1,35 @@
 package com.delta.soft_manage_system.controller;
 
+import com.delta.soft_manage_system.dto.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 @Controller
 public class TestController {
 
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @GetMapping("test")
     @ResponseBody
     public String test(){
+        try {
+            String string = jdbcTemplate.queryForObject("select userid from twei_user where userid = '1'", String.class);
+            User user = new User();
+            user.setUserid(string);
+            return user.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return "get connection failed";
     }
 }
---
+++ src/main/java/com/delta/soft_manage_system/dto/User.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: User
+ * Author:   anywhere
+ * Date:     3/18 0018 21:22
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.delta.soft_manage_system.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author anywhere
+ * @create 3/18 0018
+ * @since 1.0.0
+ */
+
+@Data
+public class User {
+    private String userid;
+    private String user_name;
+}
